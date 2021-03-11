a530435d3ce6acc0fb3d880d0c7cc43840ba2bda
Pablo FM <pablo@example.com>
2021-03-11 15:55:48 +0000

    web server ready

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var cool = require("cool-ascii-faces");
+var express= require("express");
+var app = express();
 
-console.log(cool());
+var port = 10000;
+
+app.get("/cool",(request,response) => {
+        response.send(cool());
+        console.log("New request to /cool has arrived");
+});
+
+app.listen(port,() =>{
+        console.log("Server ready listening on port "+port);
+});
